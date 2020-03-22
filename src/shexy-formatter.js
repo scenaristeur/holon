26097c34356a0e64ac22bf2fb29d9f7bfdc7f164
David <scenaristeur@gmail.com>
2020-03-22 12:36:26 +0100

    change root ttl to :this

--- src/shexy-formatter.js
+++ src/shexy-formatter.js
@@ -35,7 +35,7 @@ class ShexyFormatter extends LitElement {
     });
     if (changedProperties.has('data')){
       if(Object.keys(this.data).length > 0){
-          this.processData()
+        this.processData()
       }
     }
     return  changedProperties.has('data') || changedProperties.has('ttl');
@@ -44,13 +44,15 @@ class ShexyFormatter extends LitElement {
   processData(){
     console.log(this.data)
     var ttlFile  = {}
-    var ttlString = "@prefix : <https://holacratie.solid.community/public/> .\n"
+    var ttlString = "@prefix : <#>.\n"
+  //  +  "@prefix : <https://holacratie.solid.community/public/> .\n"
     +  "@prefix owl: <http://www.w3.org/2002/07/owl#> .\n"
     +  "@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .\n"
     +  "@prefix xml: <http://www.w3.org/XML/1998/namespace> .\n"
     +  "@prefix xsd: <http://www.w3.org/2001/XMLSchema#> .\n"
     +  "@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .\n"
-    +  "@base <https://holacratie.solid.community/public/> .\n\n\n";
+    +  "@prefix purl: <http://purl.org/dc/elements/1.1/>.\n"
+    +  "@base <"+this.shape.url+"> .\n\n\n";
 
 
     for (let [shape, data] of Object.entries(this.data)) {
@@ -72,13 +74,15 @@ class ShexyFormatter extends LitElement {
           if ((predicate == "https://schema.org/name") &&  (object.value.length > 0)){
             var underName  = object.value.split(' ').join('_');
             filename = underName;
+            ttlString += ':this  rdfs:label  "'+object.value+'".\n'
+            ttlString += ':this  purl:title  "'+object.value+'".\n'
           }
           console.log(predicate, object);
-let objectValue = object.value.startsWith("http") ? '<'+object.value+'>': '"'+object.value+'"';
-          ttlString += '<>  <'+predicate+'>  '+objectValue+'.  # Format :'+object.type+ " "+object.format+ "\n";
+          let objectValue = object.value.startsWith("http") ? '<'+object.value+'>': '"'+object.value+'"';
+          ttlString += ':this  <'+predicate+'>  '+objectValue+'.  # Format :'+object.type+ " "+object.format+ "\n";
         }
       }
-ttlString += '<> rdf:type <'+this.shape.url+'>.'
+      ttlString += ':this  rdf:type  <'+this.shape.url+'>.'
 
       const d = new Date();
       var now = d.toUTCString()+"\n";
@@ -107,33 +111,33 @@ ttlString += '<> rdf:type <'+this.shape.url+'>.'
 }
 
 
-  firstUpdated(){
-    var app = this;
-    this.agent = new HelloAgent(this.name);
-    console.log(this.agent)
-    this.agent.receive = function(from, message) {
-      //  console.log("messah",message)
-      if (message.hasOwnProperty("action")){
-        //  console.log(message)
-        switch(message.action) {
-          case "webIdChanged":
-          app.webIdChanged(message.webId)
-          break;
-          default:
-          console.log("Unknown action ",message)
-        }
+firstUpdated(){
+  var app = this;
+  this.agent = new HelloAgent(this.name);
+  console.log(this.agent)
+  this.agent.receive = function(from, message) {
+    //  console.log("messah",message)
+    if (message.hasOwnProperty("action")){
+      //  console.log(message)
+      switch(message.action) {
+        case "webIdChanged":
+        app.webIdChanged(message.webId)
+        break;
+        default:
+        console.log("Unknown action ",message)
       }
-    };
-  }
-
-  webIdChanged(webId){
-    this.webId = webId
-    if (webId != null){
-      this.updateProfile();
-    }else{
-
     }
+  };
+}
+
+webIdChanged(webId){
+  this.webId = webId
+  if (webId != null){
+    this.updateProfile();
+  }else{
+
   }
+}
 
 
 
